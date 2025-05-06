be6b1012616ab3fd396c419d4248c12854007865
namdeeptry <namnam290703@gmail.com>
2025-05-06 19:22:35 +0700

    Init app wpf

---
+++ Haircut Management/App/WPF/APP/APP/View/InvoicesView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using APP.ViewModel;
+
+namespace APP.View
+{
+    /// <summary>
+    /// Interaction logic for InvoicesView.xaml
+    /// </summary>
+    public partial class InvoicesView : Window
+    {
+        public InvoicesView()
+        {
+            InitializeComponent();
+            DataContext = new InvoicesViewModel();
+        }
+    }
+}
