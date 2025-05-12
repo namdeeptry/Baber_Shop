7a45137e9b4a780f89e12663d30674dae39ba728
namdeeptry <namnam290703@gmail.com>
2025-05-12 16:51:10 +0700

    Update UI Login for APP

---
+++ Haircut Management/App/WPF/APP/APP/UserControls/MyTextbox.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APP.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MyTextbox.xaml
+    /// </summary>
+    public partial class MyTextbox : UserControl
+    {
+        public MyTextbox()
+        {
+            InitializeComponent();
+        }
+
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        public static readonly DependencyProperty HintProperty =
+            DependencyProperty.Register("Hint", typeof(string), typeof(MyTextbox));
+
+    }
+}
